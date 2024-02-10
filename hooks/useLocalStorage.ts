b12ffceeb1ff44092c836094aa385846533fd752
puremoru <i.kawamitsu.duckfeather@gmail.com>
2024-02-10 18:42:41 +0900

    :tada: Initial Commit

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+const getLocalStorageValue = (key: string, initValue: any) => {
+  if (typeof window === "undefined") return initValue;
+
+  const item = localStorage.getItem(key);
+  return item ? item : initValue;
+};
+
+const useLocalStorage = (key: string, initValue: any) => {
+  const [value, setValue] = useState(() =>
+    getLocalStorageValue(key, initValue)
+  );
+
+  console.log(value);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
+
+  return [value, setValue] as const;
+};
+
+export default useLocalStorage;
